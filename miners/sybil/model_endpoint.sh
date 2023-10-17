4ff7ca0de97dc7595e258d246a22f182672ed5b6
Carro <robertmyers@hey.com>
2023-10-16 23:10:22 -0500

    working on minigpt4 server

--- miners/sybil/model_endpoint.sh
+++ miners/sybil/model_endpoint.sh
@@ -1,2 +1,4 @@
-python -m vllm.entrypoints.api_server \
---model teknium/OpenHermes-2-Mistral-7B 
+model=teknium/OpenHermes-2-Mistral-7B 
+volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
+
+docker run --gpus all --shm-size 1g -p 8000:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:1.1.0 --model-id $model
