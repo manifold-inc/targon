73c7aeecab2895ded1a45ec6584212a1120e72ce
Carro <robertmyers@hey.com>
2023-10-16 23:55:03 -0500

    working on minigpt4 server

--- miners/sybil/model_endpoint.sh
+++ miners/sybil/model_endpoint.sh
@@ -1,4 +1,4 @@
 model=teknium/CollectiveCognition-v1.1-Mistral-7B
 volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
 
-docker run --gpus all --net=host --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:1.1.0 --model-id $model
+docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:1.1.0 --model-id $model
