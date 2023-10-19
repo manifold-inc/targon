ab790698321649178876bac7bbb10f489670f229
Carro <robertmyers@hey.com>
2023-10-19 14:29:29 -0500

    working on minigpt4 server

--- targon/miner/search/query.py
+++ targon/miner/search/query.py
@@ -9,7 +9,7 @@ class QueryParams(BaseModel):
     hl: str
     gl: str
     google_domain: str
-    api_key: str = os.environ('SERP_API_KEY') if os.environ('SERP_API_KEY') else None
+    api_key: str = os.environ['SERP_API_KEY'] if os.environ['SERP_API_KEY'] else None
 
 
         
