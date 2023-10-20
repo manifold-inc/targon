381f7733faff5f89ade4434d34361147251345ea
Carro <robertmyers@hey.com>
2023-10-20 18:04:05 -0500

    update

--- targon/validator/reward/dpo.py
+++ targon/validator/reward/dpo.py
@@ -25,7 +25,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class DirectPreferenceRewardModel(BaseRewardModel):
 
-    reward_model_name: str = "cerebras/btlm-3b-8k-base"
+    reward_model_name: str = "OpenAssistant/reward-model-deberta-v3-large-v2"
 
     @property
     def name(self) -> str: return RewardModelType.dpo.value
@@ -36,68 +36,13 @@ class DirectPreferenceRewardModel(BaseRewardModel):
         self.penalty = 1.2 # Same penalty as the original [paper](https://arxiv.org/pdf/1909.05858.pdf).
         self.tokenizer = AutoTokenizer.from_pretrained(DirectPreferenceRewardModel.reward_model_name)
         self.model = AutoModelForCausalLM.from_pretrained(DirectPreferenceRewardModel.reward_model_name,
-                                                          trust_remote_code=True,
                                                           torch_dtype=torch.float16).to(self.device)
 
-    def reward_single(self, prompt: str, completion: str, name: str ,with_penalty=True) -> float:
-        r""" Calculates a direct preference optimization (DPO) style reward for a completion,
-        which is a reference model's average log-probability for completion tokens given a prompt.
-        Uses guidance from https://github.com/eric-mitchell/direct-preference-optimization/blob/main/trainers.py.
-        """
+    def reward_single( self, prompt: str, completion: str, name: str ) -> float:
         with torch.no_grad():
-
-            # Check if completion is 
-            if completion.strip() == '' or len(completion) <= 5:
-                return -11 # exp(-11)=1.67e-5 < 2e-5=1/50257 (typical vocab size)
+            inputs = self.tokenizer(prompt, completion, return_tensors='pt').to(self.device)
+            return float( self.model( **inputs ).logits[0].cpu().detach() )
             
-            # Tokenize the combined prompt + completion.
-            combined = self.tokenizer(prompt + completion, return_tensors="pt").input_ids[0].to(self.device)  # [seq_len]
-            # Tokenize only the prompt, to help determine prompt token length.
-            prompt_part = self.tokenizer(prompt, return_tensors="pt").input_ids[0].to(self.device)  # [prompt_len]
-
-            # Completion doesn't fit into model sequence, so return lowest reward.
-            if self.tokenizer.model_max_length <= len(prompt_part):
-                return -11.  # exp(-11)=1.67e-5 < 2e-5=1/50257 (typical vocab size)
-
-            # Truncate combined to fit into model max sequence length.
-            if self.tokenizer.model_max_length < len(combined):
-                combined = combined[:self.tokenizer.model_max_length]
-
-            labels = combined.clone()  # [seq_len]
-            # Ignore prompt part for calculating reward.
-            labels[:len(prompt_part)] = -100
-            # Label only each next token prediction ground-truth.
-            labels = labels[1:]  # [seq_len-1]
-            loss_mask = (labels != -100)  # [seq_len-1]
-            # Dummy token to allow for indexing, but loss will be ignored.
-            labels[labels == -100] = 0
-            # Reshape for gather operation.
-            labels = labels.unsqueeze(0).unsqueeze(2)  # [batch_size=1, seq_len-1, :]
-
-            # Forward pass to calculate logit predictions for each sequence position.
-            logits = self.model(combined.unsqueeze(0)).logits  # [batch_size=1, seq_len, vocab_len]
-            # Predict only where labels are available.
-            logits = logits[:, :-1, :]  # [batch_size=1, seq_len-1, vocab_len]
-
-            if with_penalty:
-                # Apply penalty for repeated generation
-                for i in range(len(prompt_part)+1, len(combined)-1):
-                    logit = logits[:,i,:].clone()
-                    inputs = combined[len(prompt_part):i].clone()
-                    logits[:,i,:] =  self.logit_penalty(input_ids=inputs, logit=logit)
-
-            # Rescale via log(softmax(logits)).
-            logits = logits.log_softmax(-1)
-            # Calculate the model's log-probability for each actual completion token.
-            per_token_logps = torch.gather(logits, dim=2, index=labels).squeeze(2)  # [batch_size=1, seq_len-1]
-            # Average log-probability over completion sequence.
-            reward = (per_token_logps * loss_mask).sum(-1) / loss_mask.sum(-1)  # [batch_size=1]
-            reward = reward[0].cpu().detach()
-
-            # NaNs can possibly arise through log(0)=-inf, replace with suitably small logits.
-            if torch.isnan(reward) or torch.isinf(reward):
-                return -11.  # exp(-11)=1.67e-5 < 2e-5=1/50257 (typical vocab size)
-            return reward.item()
         
     def get_rewards(self, prompt: str, completions: List[str], name: str) -> torch.FloatTensor:
         rewards = torch.tensor([self.reward_single(prompt, completion, name) for completion in completions],
