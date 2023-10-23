15edd62e75ffcd511d31f1b2b97a3f535658ddcc
Carro <robertmyers@hey.com>
2023-10-22 20:29:31 -0500

    testing auto updater

--- targon/__init__.py
+++ targon/__init__.py
@@ -17,7 +17,7 @@ print(base_path)
 raw_version = open(os.path.join(base_path, "VERSION")).read().strip()
 
 __version__ = [int(v) for v in raw_version.split(".")]
-version_split = __version__.split(".")
+version_split = raw_version.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
     + (10 * int(version_split[1]))
