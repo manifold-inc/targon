459e558c67320a6de81c116b81cc13c3fecd9a5f
Carro <robertmyers@hey.com>
2023-10-22 20:31:55 -0500

    testing auto updater

--- targon/validator/utils.py
+++ targon/validator/utils.py
@@ -20,45 +20,45 @@ def autoupdate():
     '''
     Updates the targon codebase if a newer version is available.
     '''
-    try:
-        bt.logging.trace("Checking for updates...")
-        response = requests.get(
-            "https://raw.githubusercontent.com/manifold-inc/targon/main/VERSION"
-        )
-        response.raise_for_status()
-        try:
-            repo_version = response.content.decode()
-            
-            latest_version = [int(v) for v in repo_version.split(".")]
-            bt.logging.debug(f"Current version: {__version__}")
-            bt.logging.debug(f"Latest version: {latest_version}")
-            if latest_version > __version__:
-                bt.logging.trace("A newer version of Targon is available. Downloading...")
-                # download latest version with git pull
+    # try:
+    bt.logging.trace("Checking for updates...")
+    response = requests.get(
+        "https://raw.githubusercontent.com/manifold-inc/targon/main/VERSION"
+    )
+    response.raise_for_status()
+    # try:
+    repo_version = response.content.decode()
+    
+    latest_version = [int(v) for v in repo_version.split(".")]
+    bt.logging.debug(f"Current version: {__version__}")
+    bt.logging.debug(f"Latest version: {latest_version}")
+    if latest_version > __version__:
+        bt.logging.trace("A newer version of Targon is available. Downloading...")
+        # download latest version with git pull
 
-                # step backwards in the path until we find targon
-                while os.path.basename(base_path) != "targon":
-                    base_path = os.path.dirname(base_path)
-                
+        # step backwards in the path until we find targon
+        while os.path.basename(base_path) != "targon":
+            base_path = os.path.dirname(base_path)
+        
 
-                os.system(f"cd {base_path} && git pull")
-                # checking local VERSION
-                with open(base_path, "VERSION") as f:
-                    new__version__ = f.read().strip()
-                    # convert to list of ints
-                    new__version__ = [int(v) for v in new__version__.split(".")]
-                    if new__version__ == latest_version:
-                        bt.logging.debug("Targon updated successfully.")
-                        bt.logging.debug("Restarting...")
-                        bt.logging.debug(f"Running: {sys.executable} {sys.argv}")
-                        os.execv(sys.executable, [sys.executable] + sys.argv)
-                    else:
-                        bt.logging.error("Targon git pull failed you will need to manually update and restart for latest code.")
-        except Exception as e:
-            bt.logging.error("Failed to convert response to json: {}".format(e))
-            bt.logging.trace("Response: {}".format(response.text))            
-    except Exception as e:
-        bt.logging.error("Failed to check for updates: {}".format(e))
+        os.system(f"cd {base_path} && git pull")
+        # checking local VERSION
+        with open(base_path, "VERSION") as f:
+            new__version__ = f.read().strip()
+            # convert to list of ints
+            new__version__ = [int(v) for v in new__version__.split(".")]
+            if new__version__ == latest_version:
+                bt.logging.debug("Targon updated successfully.")
+                bt.logging.debug("Restarting...")
+                bt.logging.debug(f"Running: {sys.executable} {sys.argv}")
+                os.execv(sys.executable, [sys.executable] + sys.argv)
+            else:
+                bt.logging.error("Targon git pull failed you will need to manually update and restart for latest code.")
+    #     except Exception as e:
+    #         bt.logging.error("Failed to convert response to json: {}".format(e))
+    #         bt.logging.trace("Response: {}".format(response.text))            
+    # except Exception as e:
+    #     bt.logging.error("Failed to check for updates: {}".format(e))
 
 
 # LRU Cache with TTL
