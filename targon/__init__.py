47e783e33d212944b9c039a3899e8c2c8ee08474
Carro <robertmyers@hey.com>
2023-10-22 20:50:53 -0500

    testing auto updater

--- targon/__init__.py
+++ targon/__init__.py
@@ -10,7 +10,7 @@ from .miner.search import query as search
 from .miner.search import QueryParams
 
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
