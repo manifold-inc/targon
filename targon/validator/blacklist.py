f1fa7fa47d463a43d09719dc064b914a3c746e05
Carro <robertmyers@hey.com>
2023-10-28 12:16:16 -0500

    adding coldkey blacklist for validators

--- targon/validator/blacklist.py
+++ targon/validator/blacklist.py
@@ -1,4 +1,4 @@
-
+import os 
 import bittensor as bt
 
 '''
@@ -7,7 +7,13 @@ This is a blacklist module for validators. It is used to blacklist bad faith key
 
 def blacklist( self ):
     self.blacklisted_coldkeys = []
-    with open('../blacklist.txt'):
+    # get the directory the file is in
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # step backwards one directory
+    dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+
+    with open(os.path.join(dir_path, 'threat-actors.txt'), 'r') as blacklist:
         for line in blacklist:
             self.blacklisted_coldkeys.append(line)
             bt.logging.info('blacklisting low integrity key', line)
