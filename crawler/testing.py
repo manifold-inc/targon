c37b70c1463e8ae5fa556ab0bf5430b2fb674552
Carro <robertmyers@hey.com>
2023-11-02 14:01:58 -0500

    adding crawler and updating vectordb admin

--- crawler/testing.py
+++ crawler/testing.py
@@ -8,7 +8,7 @@ from llm import MODEL_REGISTRY
 llm_name = "bert-base-uncased"
 llm_model = MODEL_REGISTRY[llm_name]()
 
-db_client = VectorDBClient(embed_size=llm_model.embed_size, batch_size=32)
+db_client = VectorDBClient(embed_size=llm_model.embed_size, batch_size=1)
 
 inital_url = "https://cnn.com"
 
@@ -28,11 +28,11 @@ def crawl(url, depth, max_depth):
         db_client.insert(url, full_text, embeddings)
 
         # Find and crawl child links
-        links = soup.find_all("a")
-        for link in links:
-            child_url = link.get("href")
-            if child_url and child_url.startswith("http") or child_url and child_url.startswith("https"):
-                # print(child_url)
-                crawl(child_url, depth + 1, max_depth)
+        # links = soup.find_all("a")
+        # for link in links:
+        #     child_url = link.get("href")
+        #     if child_url and child_url.startswith("http") or child_url and child_url.startswith("https"):
+        #         # print(child_url)
+        #         crawl(child_url, depth + 1, max_depth)
 
 crawl(inital_url, 0, 10)
