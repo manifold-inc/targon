9c8cb158adad692ace89c47b68cd63cfa8d97596
Carro <robertmyers@hey.com>
2024-01-26 17:43:14 -0600

    trying other strategies without PR for axon.start()

--- targon/base/prover.py
+++ targon/base/prover.py
@@ -112,7 +112,7 @@ class BaseProverNeuron(BaseNeuron):
         self.axon.serve(netuid=self.config.netuid, subtensor=self.subtensor)
 
         # Start  starts the prover's axon, making it active on the network.
-        self.loop.run_until_complete(self.axon.start())
+        asyncio.run(self.axon.start())
 
         bt.logging.info(f"Prover starting at block: {self.block}")
 
