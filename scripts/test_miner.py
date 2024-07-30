eb974827d8f3ea351840fa4c3db747dc770e84c9
josh <josh@mjbrowns.com>
2024-07-30 13:22:16 -0500

    fix: dont actually run vali in script

--- scripts/test_miner.py
+++ scripts/test_miner.py
@@ -7,6 +7,5 @@ if __name__ == "__main__":
     validator = Validator()
     validator.sync_metagraph()
     validator.resync_hotkeys()
-    validator.run()
     stats = validator.query_miners([0])
     print(stats)
