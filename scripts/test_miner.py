dfb53861983fddfb7e5377a5c4dbf613ffcf7b01
josh <josh@mjbrowns.com>
2024-07-30 15:30:25 -0500

    fix: spacing

--- scripts/test_miner.py
+++ scripts/test_miner.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
         for uid, stat in stats:
             wps = min(len(stat.response.split(" ")), len(ground_truth.split(" "))) / stat.total_time
             blob = f"UID: {uid}\n"
-            blob += f"\nGround Truth: {ground_truth}\n\n"
+            blob += f"Ground Truth: {ground_truth}\n\n"
             blob += f"Miner response: {stat.response}\n"
-            blob += f"WPS: {wps}\n\n"
+            blob += f"WPS: {wps}\n"
             blob += f"Total Time: {stat.total_time}\n"
             blob += f"Jaro Score: {stat.jaro_score}\n\n"
             res.write(blob)
