cb83a0915cebf3d55b6dc01760705d3139838b5a
josh <josh@mjbrowns.com>
2024-07-30 14:53:31 -0500

    fix: dont use clever import file method

--- targon/__init__.py
+++ targon/__init__.py
@@ -1,7 +1,7 @@
 from .config import *
 from .dataset import *
 
-__version__ = [line.strip() for line in open('./../VERSION').readlines()][0]
+__version__ = '2.0.6'
 
 version_split = __version__.split(".")
 __spec_version__ = (
