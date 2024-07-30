5360de030d8c42bb63d5411207bc5888cc28208a
josh <josh@mjbrowns.com>
2024-07-30 13:52:56 -0500

    fix: write scores to file

--- scripts/test_miner.py
+++ scripts/test_miner.py
@@ -1,22 +1,24 @@
 from neurons.validator import Validator
 
 
-MINER_UID = 0
+MINER_UIDS = []
 
 if __name__ == "__main__":
     validator = Validator()
     validator.sync_metagraph()
     validator.resync_hotkeys()
-    res = validator.query_miners([0])
+    res = validator.query_miners(MINER_UIDS)
 
     if res is None:
         print("No response from miner")
         exit()
     stats, ground_truth, sampling_params = res
-    for uid, stat in stats:
-        print(uid)
-        print(f"Ground Truth: {ground_truth}")
-        print(f"Miner response: {stat.response}")
-        print(f"Total Time: {stat.total_time}")
-        print(f"Jaro Score: {stat.jaro_score}")
-        print()
+    with open("responses.txt", "w") as res:
+        for uid, stat in stats:
+            blob = f"{uid}\n"
+            blob += f"Ground Truth: {ground_truth}\n"
+            blob += f"Miner response: {stat.response}\n"
+            blob += f"Total Time: {stat.total_time}\n"
+            blob +=f"Jaro Score: {stat.jaro_score}\n"
+            res.write(blob)
+            print(blob)
