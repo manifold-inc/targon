e81e74a48924c532b524610cb66ec1310e0bc98a
josh <josh@mjbrowns.com>
2024-08-02 09:00:19 -0500

    fix: numeric version repr

--- targon/__init__.py
+++ targon/__init__.py
@@ -1,11 +1,11 @@
 from .config import *
 from .dataset import *
 
-__version__ = '2.2.8'
+__version__ = "2.2.8"
 
 version_split = __version__.split(".")
 __spec_version__ = (
-    (1000 * int(version_split[0]))
-    + (100 * int(version_split[1]))
+    (100000 * int(version_split[0]))
+    + (1000 * int(version_split[1]))
     + (10 * int(version_split[2]))
 )
