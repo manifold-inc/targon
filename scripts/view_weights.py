17a4f14a4d25e042a7247c56a7cb9c6a58441883
josh <josh@mjbrowns.com>
2024-08-12 16:32:03 -0500

    feat: plot weights

--- scripts/view_weights.py
+++ scripts/view_weights.py
@@ -1,8 +1,12 @@
 from neurons.validator import Validator
-
+import plotext as plt
 
 MINER_UIDS = []
 
 if __name__ == "__main__":
     validator = Validator()
-    validator.get_weights()
+    uids, weights = validator.get_weights()
+    sorted(weights)
+    plt.scatter(weights) 
+    plt.title("Weights") # to apply a title
+    plt.show()
