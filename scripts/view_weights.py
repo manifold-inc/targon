4edd78b265e08d6ee3ccdf6a2e843414255c8404
josh <josh@mjbrowns.com>
2024-08-12 16:32:49 -0500

    fix: im a bad programmer

--- scripts/view_weights.py
+++ scripts/view_weights.py
@@ -6,7 +6,7 @@ MINER_UIDS = []
 if __name__ == "__main__":
     validator = Validator()
     uids, weights = validator.get_weights()
-    sorted(weights)
+    weights = sorted(weights)
     plt.scatter(weights) 
     plt.title("Weights") # to apply a title
     plt.show()
