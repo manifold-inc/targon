fa2eaaa6c61d67a22d0b059ee60f36174c8b697e
darwich6 <adarwich2010@gmail.com>
2025-02-03 11:27:13 -0600

    fix: array return

--- neurons/miner.py
+++ neurons/miner.py
@@ -96,7 +96,7 @@ class Miner(BaseNeuron):
         # Return models the miner is running
         #
 
-        return ""
+        return []
 
     async def determine_epistula_version_and_verify(self, request: Request):
         version = request.headers.get("Epistula-Version")
