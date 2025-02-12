a4cb9688a46db1eb6f09a50006c9114e348988d4
josh <josh@mjbrowns.com>
2025-02-12 10:02:47 -0600

    fix: Async llm engine instead of non-async

--- verifier/verifier.py
+++ verifier/verifier.py
@@ -1,13 +1,14 @@
 import random
-import math
 import os
 import asyncio
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 from enum import Enum
 from typing import Dict, List, Optional, Tuple, Any, Union
-from vllm import LLM, SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.utils import random_uuid
+from vllm import AsyncLLMEngine, SamplingParams
 
 # Load the model.
 MODEL_NAME = os.getenv("MODEL", None)
@@ -22,30 +23,20 @@ PIPELINE_PARALLEL = int(os.getenv("PIPELINE_PARALLEL", 1))
 CONTEXT_LENGTH = os.getenv("CONTEXT_LENGTH", None)
 if CONTEXT_LENGTH != None:
     CONTEXT_LENGTH = int(CONTEXT_LENGTH)
-MODEL_WRAPPER = LLM(
-    model=MODEL_NAME,
-    enforce_eager=True,
-    gpu_memory_utilization=0.9,
-    tensor_parallel_size=TENSOR_PARALLEL,
-    trust_remote_code=True,
-    pipeline_parallel_size=PIPELINE_PARALLEL,
-    max_model_len=CONTEXT_LENGTH,
+MODEL_WRAPPER = AsyncLLMEngine.from_engine_args(
+    AsyncEngineArgs(
+        model=MODEL_NAME,
+        enforce_eager=True,
+        gpu_memory_utilization=0.9,
+        tensor_parallel_size=TENSOR_PARALLEL,
+        trust_remote_code=True,
+    )
 )
-TOKENIZER = MODEL_WRAPPER.get_tokenizer()
-MODEL = MODEL_WRAPPER.llm_engine.model_executor.driver_worker.model_runner.model  # type: ignore
-MODEL_NUM_PARAMS = sum(1 for _ in MODEL.parameters())
-cache_config = MODEL_WRAPPER.llm_engine.cache_config
-model_config = MODEL_WRAPPER.llm_engine.model_config
-config = MODEL_WRAPPER.llm_engine.scheduler_config
-
+model_config = MODEL_WRAPPER.engine.model_config
+MODEL_WRAPPER.engine.scheduler_config.chunked_prefill_enabled = False
 
 # Lock to ensure atomicity.
 LOCK = asyncio.Lock()
-LOCK_GENERATE = asyncio.Lock()
-
-ENDPOINTS = ["completion"]
-if TOKENIZER.chat_template is not None:
-    ENDPOINTS.append("chat")
 
 
 class RequestParams(BaseModel):
@@ -120,47 +111,36 @@ app = FastAPI()
 
 @app.post("/generate")
 async def generate_question(req: GenerateRequest):
-    async with LOCK_GENERATE:
+    async with LOCK:
         try:
-            if "chat" in ENDPOINTS:
-                output = (
-                    MODEL_WRAPPER.chat(
-                        messages=req.messages,
-                        sampling_params=SamplingParams(
-                            **req.sampling_params.model_dump()
-                        ),
-                        use_tqdm=False,  # type: ignore
-                    )[0]
-                    .outputs[0]
-                    .text
+            prompt = ""
+            for message in req.messages:
+                prompt += (
+                    message.get("role", "") + ": " + message.get("content", "") + "\n"
                 )
-            else:
-                prompt = ""
-                for message in req.messages:
-                    prompt += (
-                        message.get("role", "")
-                        + ": "
-                        + message.get("content", "")
-                        + "\n"
-                    )
-                prompt += "\nResponse: "
-                output = (
-                    MODEL_WRAPPER.generate(
-                        prompts=prompt,
-                        sampling_params=SamplingParams(
-                            **req.sampling_params.model_dump()
-                        ),
-                        use_tqdm=False,
-                    )[0]
-                    .outputs[0]
-                    .text
-                )
-            return {"text": output}
+            prompt += "\nResponse: "
+            output = MODEL_WRAPPER.generate(
+                request_id="",
+                prompt=prompt,
+                sampling_params=SamplingParams(**req.sampling_params.model_dump()),
+            )
+            final_output = None
+            try:
+                async for request_output in output:
+                    final_output = request_output
+            except asyncio.CancelledError:
+                return Response(status_code=499)
+
+            assert final_output is not None
+            prompt = final_output.prompt
+            assert prompt is not None
+            text_outputs = [prompt + output.text for output in final_output.outputs]
+            return {"text": text_outputs}
         except Exception:
             return {"text": None}
 
 
-def verify_logprobs_random(
+async def verify_logprobs_random(
     temperature: float,
     seed: int,
     input_text: str,
@@ -171,6 +151,7 @@ def verify_logprobs_random(
     """
     Generate a handful of random outputs to ensure the logprobs weren't generated after the fact.
     """
+    TOKENIZER = await MODEL_WRAPPER.get_tokenizer()
     if len(output_sequence) < 4:
         return True, "Skipped random verification due to insufficient tokens"
 
@@ -202,15 +183,25 @@ def verify_logprobs_random(
     for idx in indices_to_check:
         full_text = input_text + "".join([item.text for item in output_sequence[0:idx]])
 
-        output = MODEL_WRAPPER.generate([full_text], sampling_params, use_tqdm=False)[
-            0
-        ].outputs[0]
+        output = MODEL_WRAPPER.generate(
+            full_text, sampling_params, request_id=random_uuid()
+        )
+        final_output = None
+        try:
+            async for request_output in output:
+                final_output = request_output
+        except asyncio.CancelledError:
+            return False, "Request Canceled"
+        assert final_output is not None
+        outputs = final_output.outputs[0]
 
         top_tokens = []
-        if output.logprobs is None:
+        if outputs.logprobs is None:
             continue
 
-        for lp in output.logprobs:
+        for lp in outputs.logprobs:
+            if lp is None:
+                continue
             top_tokens += list(lp.keys())
 
         if output_sequence[idx].token_id not in top_tokens:
@@ -221,7 +212,7 @@ def verify_logprobs_random(
     return True, success_msg
 
 
-def verify_logprobs(
+async def verify_logprobs(
     temperature: float,
     seed: int,
     max_tokens: int,
@@ -235,8 +226,9 @@ def verify_logprobs(
     Compare the produced logprob values against the ground truth, or at least
     the ground truth according to this particular GPU/software pairing.
     """
+    TOKENIZER = await MODEL_WRAPPER.get_tokenizer()
     # Set up sampling parameters
-    top_logprobs = min(int(temperature * 10) + 6, 15)
+    top_logprobs = 15
     sampling_params_dict = {
         "temperature": temperature,
         "seed": seed,
@@ -252,17 +244,28 @@ def verify_logprobs(
 
     # Generate output for a single token
     output = None
+    full_text = input_text + "".join([item.text for item in output_sequence])
     for _ in range(5):
-        full_text = input_text + "".join([item.text for item in output_sequence])
-        output = MODEL_WRAPPER.generate([full_text], sampling_params, use_tqdm=False)[0]
+        output = MODEL_WRAPPER.generate(
+            prompt=full_text, sampling_params=sampling_params, request_id=random_uuid()
+        )
+        final_output = None
+        try:
+            async for request_output in output:
+                final_output = request_output
+        except asyncio.CancelledError:
+            print("Asyncio Canceled")
+            return None
+        assert final_output is not None
+        output = final_output
         if output.prompt_logprobs is not None:
             break
 
     if not output or output.prompt_logprobs is None:
+        print("Output or prompt logprobs is None")
         return None
 
     # The actual logprobs should be very close but not 100% due to GPU/driver differences
-    total_score = 0.0
     idxs = min(
         len(output.prompt_logprobs) - len(input_tokens) - 3,
         len(output_sequence) - 1,
@@ -273,15 +276,15 @@ def verify_logprobs(
     output_tokens = [item.token_id for item in output_sequence]
     really_low_prob = 0
     not_first = 0
-
+    assert output.prompt_token_ids
     for idx in range(idxs):
-        item = output_sequence[idx]
 
-        expected_logprob = output.prompt_logprobs[idx + len(input_tokens)]
-        assert expected_logprob is not None
+        expected_logprob_set = output.prompt_logprobs[idx + len(input_tokens)]
+        token_id = output.prompt_token_ids[idx + len(input_tokens)]
+        assert expected_logprob_set is not None
 
-        eos_logprob = expected_logprob.get(eos_token_id)
-        eot_logprob = expected_logprob.get(eot_token_id)
+        eos_logprob = expected_logprob_set.get(eos_token_id)
+        eot_logprob = expected_logprob_set.get(eot_token_id)
 
         if (not eos_logprob and eot_logprob) or (
             eos_logprob
@@ -292,7 +295,9 @@ def verify_logprobs(
         ):
             eos_logprob = eot_logprob
 
-        expected_logprob = expected_logprob.get(item.token_id)
+        expected_logprob = expected_logprob_set.get(token_id)
+
+        token_text = TOKENIZER.decode([token_id])
 
         if eos_logprob and (
             not expected_logprob
@@ -303,7 +308,7 @@ def verify_logprobs(
                 and expected_logprob.rank > 15
             )
         ):
-            error_msg = f"Expected EOS/EOT token at index {idx}"
+            error_msg = f"Expected EOS/EOT token at index {idx}, {token_text=}, {expected_logprob_set=}, {eos_logprob=}"
             return False, error_msg, "SKIPPED_EOS_EOT"
 
         if expected_logprob is None:
@@ -311,8 +316,6 @@ def verify_logprobs(
 
         rank = expected_logprob.rank
         assert rank is not None
-
-        token_text = TOKENIZER.decode([item.token_id])
 
         if rank >= 75:
             error_msg = f"Found extraordinarily improbable token '{token_text}' at index {idx}: {rank=}"
@@ -328,21 +331,10 @@ def verify_logprobs(
             not_first += 1
 
         expected_logprob = expected_logprob.logprob
-        produced_logprob = item.logprob
-        score = 1.0 - min(
-            1.0, abs(math.exp(expected_logprob) - math.exp(produced_logprob))
-        )
 
-        if produced_logprob == 0:
+        if expected_logprob == 0:
             perfect_tokens += 1
 
-        if score >= 0.9:
-            score = 1.0
-
-        if rank == 1 and temperature >= 0.9 and produced_logprob != 0:
-            score = 1.0
-
-        total_score += score
 
     # Check if miner produced non-top ranking tokens more than top-ranking tokens
     ratio = not_first / len(output_tokens)
@@ -373,17 +365,9 @@ def verify_logprobs(
                 )
                 return False, error_msg, "EARLY_END"
 
-    average_score = round(total_score / idxs, 5)
+
     perfect_avg = round(perfect_tokens / idxs, 5)
-
-    if average_score < LOGPROB_FAILURE_THRESHOLD:
-        error_msg = (
-            f"Average score {average_score} below threshold {LOGPROB_FAILURE_THRESHOLD}"
-        )
-        return False, error_msg, "LOW_SCORE"
-
-    passes = average_score >= LOGPROB_FAILURE_THRESHOLD
-    if passes and perfect_avg >= 1:
+    if perfect_avg >= 1:
         error_msg = f"Overfitted response tokens. {perfect_avg}% perfect"
         return False, error_msg, "OVERFIT"
 
@@ -421,9 +405,9 @@ def verify_usage(
     return True, "", ""
 
 
-def parse_token_id(token: str) -> Optional[int]:
+def parse_token_id(token: Optional[str]) -> Optional[int]:
     """Parse token string into token ID."""
-    if not token:
+    if token is None:
         return -1
 
     if token.startswith("token_id:"):
@@ -459,7 +443,7 @@ def parse_chunk(chunk: Dict, request_type: str) -> Optional[OutputItem]:
                 return None
 
             choiceprobs = choice.get("logprobs")
-            if not choiceprobs:
+            if choiceprobs is None:
                 return None
 
             # Handle tool calls
@@ -497,9 +481,11 @@ def parse_chunk(chunk: Dict, request_type: str) -> Optional[OutputItem]:
 
         elif request_type == "COMPLETION":
             text = choice.get("text")
-            logprobs = choice.get("logprobs")
+            if text is None:
+                return None
 
-            if text == None or logprobs == None:
+            logprobs = choice.get("logprobs")
+            if logprobs is None:
                 return None
 
             token = logprobs.get("tokens", [""])[0]
@@ -522,7 +508,7 @@ def parse_chunk(chunk: Dict, request_type: str) -> Optional[OutputItem]:
 @app.post("/verify")
 async def verify(request: VerificationRequest) -> Dict:
     """Verify a miner's output."""
-    output_sequence = []
+    output_sequence: List[OutputItem] = []
     input_text = None
 
     # Parse raw chunks into OutputItems
@@ -566,6 +552,7 @@ async def verify(request: VerificationRequest) -> Dict:
     reported_usage = Usage(**usage_data)
 
     # Tokenize the input sequence
+    TOKENIZER = await MODEL_WRAPPER.get_tokenizer()
     input_text = (
         request.request_params.prompt
         if request.request_type == RequestType.COMPLETION.value
@@ -610,8 +597,13 @@ async def verify(request: VerificationRequest) -> Dict:
         if not result:
             return return_value
 
+        # Pops think character for r1
+        if input_text.strip().endswith(output_sequence[1].text):
+            output_sequence.pop(1)
+        if input_text.strip().endswith(output_sequence[0].text):
+            output_sequence.pop(0)
         # Logprob checks
-        res = verify_logprobs(
+        res = await verify_logprobs(
             request.request_params.temperature,
             request.request_params.seed,
             request.request_params.max_tokens,
@@ -636,9 +628,10 @@ async def verify(request: VerificationRequest) -> Dict:
 
         # Random logprob check
         if request.request_params.temperature > 0.75:
+            print("Verified Response")
             return {"verified": True}
 
-        res = verify_logprobs_random(
+        res = await verify_logprobs_random(
             request.request_params.temperature,
             request.request_params.seed,
             str(input_text),
@@ -662,11 +655,17 @@ async def verify(request: VerificationRequest) -> Dict:
         if not result:
             return return_value
 
+        print("Verified Response")
         return {"verified": True, "input_tokens": len(input_tokens)}
 
 
 @app.get("/metadata")
-def endpoints():
+async def endpoints():
+    ENDPOINTS = ["completion"]
+    TOKENIZER = await MODEL_WRAPPER.get_tokenizer()
+    if TOKENIZER.chat_template is not None:
+        ENDPOINTS.append("chat")
+
     return {"endpoints": ENDPOINTS, "max_model_len": model_config.max_model_len}
 
 
