0ad4ffb35438272763271b330046c73b46d95bef
josh <josh@mjbrowns.com>
2025-02-13 13:42:15 -0600

    fix: .9

--- verifier/verifier.py
+++ verifier/verifier.py
@@ -28,7 +28,7 @@ if CONTEXT_LENGTH != None:
 MODEL_WRAPPER = AsyncLLMEngine.from_engine_args(
     AsyncEngineArgs(
         model=MODEL_NAME,
-        gpu_memory_utilization=0.8,
+        gpu_memory_utilization=0.9,
         tensor_parallel_size=TENSOR_PARALLEL,
         trust_remote_code=True,
         enable_chunked_prefill=False,
