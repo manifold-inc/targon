9dfb9594f51cb9d2b322e422f836d429880991ad
josh <hey@imjosh.dev>
2025-04-15 12:10:52 -0500

    feat: new subnet boilerplate

--- cmd/targon.go
+++ cmd/targon.go
@@ -2,54 +2,66 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"time"
 
 	"targon/internal/setup"
-	"targon/internal/subtensor/utils"
+	"targon/validator"
 
 	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
-	"github.com/subtrahend-labs/gobt/runtime"
 )
 
 func main() {
 	deps := setup.Init()
 	deps.Log.Infof("Starting validator with key [%s] on chain [%s]", deps.Env.HOTKEY_SS58, deps.Env.CHAIN_ENDPOINT)
 
-	blockHash, err := deps.Client.Api.RPC.Chain.GetBlockHashLatest()
-	if err != nil {
-		deps.Log.Fatalf("Error getting latest block hash: %s", err)
-	}
+	validator := validator.NewValidator(4)
+	deps.Log.Infof("Creating validator on netuid [%d]", validator.NetUID)
+	validator.AddBlockCallback(func(h types.Header) {
+		deps.Log.Infow("New block", "block", fmt.Sprintf("%v", h.Number))
+	})
+	validator.SetOnSubscriptionCreationError(func(e error) {
+		deps.Log.Infow("Creation Error", "error", e)
+		panic(e)
+	})
+	validator.SetOnSubscriptionError(func(e error) {
+		deps.Log.Infow("Subscription Error", "error", e)
+		panic(e)
+	})
+	validator.SetMainFunc(func(i <-chan bool, o chan<- bool) {
+		for {
+			select {
+			case <-i:
+				deps.Log.Info("See shutdown call")
+				time.Sleep(5 * time.Second)
+				o <- true
+				return
+			default:
+				deps.Log.Info("Running....")
+				time.Sleep(2 * time.Second)
+			}
+		}
+	})
+	validator.Start(deps.Client)
 
-	netuid := 4
-	deps.Log.Infof("Testing netuid [%d]", netuid)
-	neurons, err := runtime.GetNeurons(deps.Client, uint16(netuid), &blockHash)
-	if err != nil {
-		deps.Log.Infof("Error fetching neurons for netuid [%d]: [%s]", netuid, err)
-	}
+	//neurons, err := runtime.GetNeurons(deps.Client, uint16(netuid), &blockHash)
+	//if err != nil {
+	//	deps.Log.Infof("Error fetching neurons for netuid [%d]: [%s]", netuid, err)
+	//}
 
-	if len(neurons) == 0 {
-		deps.Log.Infof("No neurons found for netuid [%d]", netuid)
-	}
-
-	deps.Log.Infof("total of %d neurons", len(neurons))
-	for i, neuron := range neurons {
-		deps.Log.Infow(
-			"neuron",
-			"Neuron",
-			strconv.Itoa(i),
-			"Hotkey",
-			utils.AccountIDToSS58(neuron.Hotkey),
-			"Coldkey",
-			utils.AccountIDToSS58(neuron.Coldkey),
-			"UID",
-			fmt.Sprintf("%d", neuron.UID.Int64()),
-			"Active",
-			fmt.Sprintf("%v", neuron.Active == types.NewBool(true)),
-		)
-	}
-	deps.Log.Sync()
-	sub, _ := deps.Client.Api.RPC.Chain.SubscribeNewHeads()
-	for head := range sub.Chan() {
-		deps.Log.Infow("New block", "block", fmt.Sprintf("%v", head.Number))
-	}
+	//deps.Log.Infof("total of %d neurons", len(neurons))
+	//for i, neuron := range neurons {
+	//	deps.Log.Infow(
+	//		"neuron",
+	//		"Neuron",
+	//		strconv.Itoa(i),
+	//		"Hotkey",
+	//		utils.AccountIDToSS58(neuron.Hotkey),
+	//		"Coldkey",
+	//		utils.AccountIDToSS58(neuron.Coldkey),
+	//		"UID",
+	//		fmt.Sprintf("%d", neuron.UID.Int64()),
+	//		"Active",
+	//		fmt.Sprintf("%v", neuron.Active == types.NewBool(true)),
+	//	)
+	//}
 }
