ffec8a50a859d09b642d9f0e599339a47578ebcd
josh <hey@imjosh.dev>
2025-05-06 15:46:44 -0500

    feat: get neuron script

--- targon/cmd/debug/main.go
+++ targon/cmd/debug/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"flag"
+
 	"targon/internal/setup"
+	"targon/internal/targon"
+
+	"github.com/subtrahend-labs/gobt/runtime"
 )
 
 func main() {
@@ -12,6 +17,23 @@ func main() {
 		deps.Env.CHAIN_ENDPOINT,
 		deps.Env.VERSION,
 	)
+	core := targon.CreateCore(deps)
 
-	//core := targon.CreateCore(deps)
+	numbPtr := flag.Int("uid", -1, "Miner uid")
+	flag.Parse()
+	if *numbPtr == -1 {
+		core.Deps.Log.Fatal("No UID selected, exiting")
+	}
+
+	blockHash, err := core.Deps.Client.Api.RPC.Chain.GetBlockHashLatest()
+	if err != nil {
+		core.Deps.Log.Errorw("Failed getting blockhash for neurons", "error", err)
+		return
+	}
+	neuron, err := runtime.GetNeuron(core.Deps.Client, uint16(core.Deps.Env.NETUID), uint16(224), &blockHash)
+	if err != nil {
+		core.Deps.Log.Errorw("Failed getting neurons", "error", err)
+		return
+	}
+	core.Deps.Log.Infof("Axon Info: %+v", neuron)
 }
