1940029701791b5561d962237a6307944502f29b
josh <hey@imjosh.dev>
2025-05-07 09:28:53 -0500

    feat: debug tool for miner uids

--- targon/cmd/debug/main.go
+++ targon/cmd/debug/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
+	"time"
 
 	"targon/internal/setup"
 	"targon/internal/targon"
@@ -30,10 +33,30 @@ func main() {
 		core.Deps.Log.Errorw("Failed getting blockhash for neurons", "error", err)
 		return
 	}
-	neuron, err := runtime.GetNeuron(core.Deps.Client, uint16(core.Deps.Env.NETUID), uint16(224), &blockHash)
+	neuron, err := runtime.GetNeuron(core.Deps.Client, uint16(core.Deps.Env.NETUID), uint16(*numbPtr), &blockHash)
 	if err != nil {
 		core.Deps.Log.Errorw("Failed getting neurons", "error", err)
 		return
 	}
 	core.Deps.Log.Infof("Axon Info: %+v", neuron)
+
+	tr := &http.Transport{
+		TLSHandshakeTimeout: 5 * time.Second,
+		MaxConnsPerHost:     1,
+		DisableKeepAlives:   true,
+	}
+	client := &http.Client{Transport: tr, Timeout: 5 * time.Minute}
+
+	nodes, err := targon.GetCVMNodes(core, client, neuron)
+	if err != nil {
+		core.Deps.Log.Fatalf("Failed to get nodes: %s", err)
+	}
+	for _, n := range nodes {
+		gpus, _, err := targon.CheckCVMAttest(core, client, neuron, n)
+		if err != nil {
+			core.Deps.Log.Infow("CVM attest error", "err", err)
+			continue
+		}
+		core.Deps.Log.Infow("CVM attest results", "node", n, "gpus", fmt.Sprintf("%v", gpus))
+	}
 }
