688d5cc5879cc1b20c8263e24b2713a380fa523b
josh <hey@imjosh.dev>
2025-10-18 14:07:49 -0500

    fix: lock 9.11.1

--- weights-api/main.py
+++ weights-api/main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import List
+from typing import List, Union
 from bittensor.core.axon import traceback, uvicorn
 from pydantic import BaseModel
 import bittensor as bt
@@ -47,7 +47,7 @@ logger.info(
 
 class WeightRequest(BaseModel):
     uids: List[int]
-    weights: List[int]
+    weights: List[Union[int, float]]
     version: int
 
 
